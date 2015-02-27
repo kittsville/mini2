eb293aa3763473b40e50cd6209c1bf02a4858053
Kit <kittsville@googlemail.com>
2015-02-27 17:41:46 +0000

    Did a lot of stuff

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,5 @@
 class WordsController < ApplicationController
-  before_action :set_word, only: [:show, :edit, :update, :destroy]
+  before_action :set_word, only: [:show]
 
   # GET /words
   # GET /words.json
@@ -12,53 +12,8 @@ class WordsController < ApplicationController
   def show
   end
 
-  # GET /words/new
-  def new
-    @word = Word.new
-  end
-
-  # GET /words/1/edit
-  def edit
-  end
-
-  # POST /words
-  # POST /words.json
-  def create
-    @word = Word.new(word_params)
-
-    respond_to do |format|
-      if @word.save
-        format.html { redirect_to @word, notice: 'Word was successfully created.' }
-        format.json { render :show, status: :created, location: @word }
-      else
-        format.html { render :new }
-        format.json { render json: @word.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /words/1
-  # PATCH/PUT /words/1.json
-  def update
-    respond_to do |format|
-      if @word.update(word_params)
-        format.html { redirect_to @word, notice: 'Word was successfully updated.' }
-        format.json { render :show, status: :ok, location: @word }
-      else
-        format.html { render :edit }
-        format.json { render json: @word.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /words/1
-  # DELETE /words/1.json
-  def destroy
-    @word.destroy
-    respond_to do |format|
-      format.html { redirect_to words_url, notice: 'Word was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  def listwords
+    render json: Word.all
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :words
+  get '/', to: 'words#index'
+  get '/:id' => 'words#show'
+
+  class OnlyAjaxRequests
+    def matches?(request)
+      request.xhr?
+    end
+  end
+
+#matches route with AJAX post might be worthwhile to add a attribute test
+match '/' => 'words#listwords', :constraints => OnlyAjaxRequests.new, via: :post, defaults: {format: 'json'}
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
